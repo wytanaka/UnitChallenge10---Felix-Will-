8d9592c2545469b876220289ea08baa8f371049d
Will Y Tanaka <wt40173@srcs.loc>
2017-02-03 13:19:22 -0800

    More tests and minor tweaks

--- calculator_tests.js
+++ calculator_tests.js
@@ -26,21 +26,62 @@ QUnit.test("change the sign test", function ( assert ) {
 
 //US2: As a user I want to be able to press a button and change the inputed number on the calculator's screen to change to a percentage.
 
+QUnit.test("percentage test", function (assert) {
+    addDigit(".123");
+    percentage();
+    assert.equal(document.getElementById('screen').value, "12.3", "Passed - Expected 12.3");
+    allClear();
+});
 
 //US3: As a user, I want to be able to calculate the inverse of a number simply by pressing a button.
 
+QUnit.test("inverse test" , function ( assert ) {
+    addDigit("8");
+    inverse();
+    assert.equal(document.getElementById('screen').value, "0.125", "Passed - Expected 0.125");
+    allClear();
+});
 
 //US4: As a user I want to be able to calculate the factorial of a number by simply pressing a button.
+QUnit.test("factorial test" , function ( assert ) {
+    addDigit("3");
+ factorial();
+    assert.equal(document.getElementById('screen').value, "6", "Passed - Expected 6");
+    allClear();
+});
 
 
 //US5: As a user I want to be able to calculate the square root of a number by simply pressing a button.
 
+QUnit.test("square root test" , function ( assert ) {
+    addDigit("16");
+squareRoot();
+    assert.equal(document.getElementById('screen').value, "4", "Passed - Expected 4");
+    allClear();
+});
+
 
 //US6: As a user I want to be able to calculate the square of a number by simply pressing a button.
 
+QUnit.test("square test" , function ( assert ) {
+    addDigit("8");
+square();
+    assert.equal(document.getElementById('screen').value, "64", "Passed - Expected 64");
+    allClear();
+});
 
 //US7: As a user who sometimes makes mistakes when pressing buttons on the keypad, I want to be able to press a button that clears my current input, but not the stored procedure.
 
+
+QUnit.test("clear (C) test" , function ( assert ) {
+    addDigit("854312");
+    storeOperator("*");
+cancel();
+    addDigit("6");
+    addDigit("4");
+    assert.equal(document.getElementById('screen').value, "64", "Passed - Expected 64");
+    allClear();
+});
 
 //US8: Bug Alert! There is a bug in the calculator app! As a careless user I want to be told that I just tried to divide by zero, which I should be told is not allowed.
 
